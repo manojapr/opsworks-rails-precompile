c557aafb71c901760e1f323938632ea6b85e2617
manojapr <manoj.pr247@gmail.com>
2017-05-03 11:30:45 +0200

    updated

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,13 +1,34 @@
+include_recipe "deploy"
+
 node[:deploy].each do |application, deploy|
-  rails_env = deploy[:rails_env]
-  current_path = deploy[:current_path]
+  Chef::Log.warn("This is a test!")
 
-  Chef::Log.info("Precompiling Rails assets with environment #{rails_env}")
+  if deploy[:application_type] != 'rails'
+    Chef::Log.warn("Skipping precompile_assets::deploy application #{application} as it is not an Rails app")
+    next
+  end
 
-  execute 'rake assets:precompile' do
-    cwd current_path
-    user 'deploy'
-    command 'bundle exec rake assets:precompile'
-    environment 'RAILS_ENV' => rails_env
+  unless File.exist?(File.join(deploy[:current_path], 'app', 'assets'))
+    Chef::Log.warn("Skipping precompile_assets::deploy application #{application} as no assets folder exists")
+    next
+  end
+  
+  directory "#{deploy[:deploy_to]}/shared/assets" do
+    group deploy[:group]
+    owner deploy[:user]
+    mode "0775"
+    action :create
+    recursive true
+  end
+
+  execute "Link shared asset folder for #{application}" do
+    command "ln -nfs #{deploy[:deploy_to]}/shared/assets #{deploy[:absolute_document_root]}/assets"
+    user deploy[:user]
+  end
+
+  execute "Precompile assets for #{application}" do
+    cwd deploy[:current_path]
+    command "bundle exec rake assets:precompile"
+    user deploy[:user]
   end
 end
